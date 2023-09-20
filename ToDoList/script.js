658e88aaa8ca9ba66d248bd05d1d3a569b0336bf
Márcio Alex <87771095+MarcioAlex-x@users.noreply.github.com>
2023-09-20 01:37:23 -0300

    Add files via upload
    Não deu tempo de finalizar estilo

---
+++ ToDoList/script.js
@@ -0,0 +1,106 @@
+// Selecionando elementos
+
+const todoForm = document.querySelector('#todo-form')
+const todoInput = document.querySelector('#todo-input')
+const todoList = document.querySelector('#todo-list')
+const editForm = document.querySelector('#edit-form')
+const editInput = document.querySelector('#edit-input')
+const cancelEditBtn = document.querySelector('#cancel-edit-btn')
+const info = document.querySelector('#info')
+
+let oldInputValue
+
+// Funções
+
+const saveTodo = (text) =>{
+    const todo = document.createElement('div')
+    todo.classList.add('todo')
+    const todoTitle = document.createElement('h3')
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement('button')
+    doneBtn.classList.add('finish-todo')
+    doneBtn.innerHTML = 'Feito'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit-todo')
+    editBtn.innerHTML = 'Editar '
+    todo.appendChild(editBtn)
+
+    const removeBtn = document.createElement('button')
+    removeBtn.classList.add('remove-todo')
+    removeBtn.innerHTML = 'Remover'
+    todo.appendChild(removeBtn)
+
+    todoList.appendChild(todo)
+    todoInput.value = ''
+    todoInput.focus()
+
+}
+
+const toggleForms = () =>{
+    editForm.classList.toggle('hide')
+    todoForm.classList.toggle('hide')
+    todoList.classList.toggle('hide')
+} 
+
+// Eventos
+todoForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const inputValue = todoInput.value
+    if (inputValue){
+        saveTodo(inputValue)
+    }
+    
+    info.classList.toggle('hide')
+})
+
+updateTodo = (text)=>{
+    const todos = document.querySelectorAll('.todo')
+    todos.forEach((todo)=>{
+        let todoTitle = todo.querySelector('h3')
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text
+        }
+    })
+}
+
+document.addEventListener('click',(e)=>{
+    const targetEl = e.target
+    const parentEl = targetEl.closest('div')
+    let todoTitle
+
+    if(parentEl && parentEl.querySelector('h3')){
+        todoTitle = parentEl.querySelector('h3').innerText
+    }
+
+    if(targetEl.classList.contains('finish-todo')){
+        parentEl.classList.toggle('done')
+    }
+
+    if(targetEl.classList.contains('remove-todo')){
+        parentEl.remove()
+    }
+
+    if(targetEl.classList.contains('edit-todo')){
+        toggleForms()
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+    }
+})
+
+cancelEditBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    toggleForms()
+})
+
+editForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const editInputValue = editInput.value
+    if(editInputValue){
+        updateTodo(editInputValue)
+    }
+    toggleForms()
+})
